838672c4024210e55d1ea686e76fd5841ac1e135
Kerry Jiang <kerry-jiang@hotmail.com>
2012-10-08 10:47:13 +0800

    added build script

---
+++ Build.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set fdir=%WINDIR%\Microsoft.NET\Framework
+set msbuild=%fdir%\v4.0.30319\msbuild.exe
+
+%msbuild% DynamicViewModel\DynamicViewModel.csproj /p:Configuration=Release /t:Rebuild /p:OutputPath=..\bin\Net40
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+%msbuild% DynamicViewModel.SL\DynamicViewModel.SL.csproj /p:Configuration=Release /t:Rebuild /p:OutputPath=..\bin\SL
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+pause
