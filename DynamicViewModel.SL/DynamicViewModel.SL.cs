4ce4ea99a343d83823ef2f9e7042d74e2eb5ec44
Kerry Jiang <kerry-jiang@hotmail.com>
2012-10-11 15:53:54 +0800

    improved silverlight version

---
+++ DynamicViewModel.SL/DynamicViewModel.SL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+using System.Reflection;
+
+namespace DynamicViewModel
+{
+    public partial class DynamicViewModel
+        : DynamicObject, INotifyPropertyChanged
+    {
+        public object this[string index]
+        {
+            get
+            {
+                object value;
+                _propertyValues.TryGetValue(index, out value);
+                return value;
+            }
+
+            set
+            {
+                Set(index, value);
+                RaisePropertyChanged("[" + index + "]");
+            }
+        }
+    }
+}
